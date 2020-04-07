3f8b9af2418b171669d6126b1fc3713e6cb855f8
LiuQ-Q <545784977@qq.com>
2020-04-07 18:49:05 +0800

    connect to mysql and react-blog

---
+++ app/controller/default/home.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    this.ctx.body = '123123123';
+  }
+
+  async getArticleList() {
+    const sql = 'SELECT article.id as id,' +
+              'article.title as title,' +
+              'article.introduce as introduce,' +
+              "FROM_UNIXTIME(article.add_time, '%Y-%m-%d %H:%i:%s') as addTime," +
+              'article.view_count as viewCount,' +
+              'type.name as typeName ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.id';
+
+    const results = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: results,
+    };
+  }
+
+  async getArticleById() {
+    const id = this.ctx.params.id;
+    const sql = 'SELECT article.id as id,' +
+              'article.title as title,' +
+              'article.introduce as introduce,' +
+              'article.content as articleContent,' +
+              "FROM_UNIXTIME(article.add_time, '%Y-%m-%d %H:%i:%s') as addTime," +
+              'article.view_count as viewCount,' +
+              'type.id as typeId,' +
+              'type.name as typeName ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.id ' +
+              'WHERE article.id=' + id;
+
+    const result = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: result,
+    };
+  }
+
+  async getTypeInfo() {
+    const result = await this.app.mysql.select('type');
+    this.ctx.body = { data: result };
+  }
+
+  async getArticleListByTypeId() {
+    const id = this.ctx.params.id;
+    const sql = 'SELECT article.id as id,' +
+              'article.title as title,' +
+              'article.introduce as introduce,' +
+              "FROM_UNIXTIME(article.add_time, '%Y-%m-%d %H:%i:%s') as addTime," +
+              'article.view_count as viewCount,' +
+              'type.name as typeName ' +
+              'FROM article LEFT JOIN type ON article.type_id = type.id ' +
+              'WHERE article.type_id=' + id;
+
+    const results = await this.app.mysql.query(sql);
+    this.ctx.body = {
+      data: results,
+    };
+  }
+}
+
+module.exports = HomeController;
---
+++ config/config.default.js
@@ -0,0 +1,56 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1586073636935_7687';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+
+  config.mysql = {
+    client: {
+      host: 'localhost',
+      port: '3306',
+      user: 'root',
+      password: 'lq100f',
+      database: 'react_blog',
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+
+  config.security = {
+    scrf: {
+      enable: false,
+    },
+    domainWhiteList: [ '*' ],
+  };
+
+  config.cors = {
+    origin: '*',
+    allowMethods: 'GET,HEAD,PUT,POST,UPDATE,DELETE,PATCH,OPTIONS',
+  };
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
